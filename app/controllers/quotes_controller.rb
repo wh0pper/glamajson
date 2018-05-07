29dba33053452c0c5cc65f00822547c2078ef3ce
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-05-07 11:07:25 -0700

    add exception handlings and success messages

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,11 +1,39 @@
 class QuotesController < ApplicationController
+  include Response
+
   def index
-    @quotes = {"quotation": "That's Funny, Tell Another One"}
-    json_response(@quotes)
+    @quotes = Quote.all
+    json_response(@quotes, :ok)
+  end
+
+  def show
+    @quote = Quote.find(params[:id])
+    json_response(@quote, :ok)
+  end
+
+  def create
+    @quote = Quote.create!(quote_params)
+    json_response(@quote, :created)
+  end
+
+  def update
+    @quote = Quote.find(params[:id])
+    if @quote.update!(quote_params)
+      render status: 200, json: {
+        message: "Quote successfully updated."
+      }
+  end
+
+  def destroy
+    @quote = Quote.find(params[:id])
+    if @quote.destroy!
+      render status: 200, json: {
+        message: "Quote successfully destroyed."
+      }
   end
 
   private
-  def json_response(object)
-    render json: object, status: :ok
+  def quote_params
+    params.permit(:author, :content)
   end
-end 
+end
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,3 +1,4 @@
 class Quote < ApplicationRecord
-
+  validates :content, presence: true
+  validates :author, presence: true
 end
