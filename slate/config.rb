61989faeda5f6ff217915bcc018d9b3163419e4d
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-05-21 14:33:32 -0700

    successfully build docs into public

--- slate/config.rb
+++ slate/config.rb
@@ -39,7 +39,7 @@ set :relative_links, true
 
 # Build Configuration
 configure :build do
-  set :build_dir, '../app/views/welcome'
+  set :build_dir, '../public'
   # If you're having trouble with Middleman hanging, commenting
   # out the following two lines has been known to help
   activate :minify_css
