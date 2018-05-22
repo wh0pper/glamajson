b2cac0a153a381d261c949da09180d124e234ada
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-05-21 18:13:19 -0700

    add rupaul to seed

--- db/seeds/rupaul_quotes.seeds.rb
+++ db/seeds/rupaul_quotes.seeds.rb
@@ -1,5 +1,9 @@
+rupaul = Queen.create!(name: 'RuPaul Charles', real_name: 'RuPaul Charles', age: 57, city: "Los Angeles, CA")
+Season.all.each do |s|
+  s.queens << rupaul
+end
 
 20.times do |i|
-  quote = Quote.create!(content: Faker::RuPaul.quote)
+  Quote.create!(content: Faker::RuPaul.quote)
 end
 puts "Generated #{Quote.all.length} quotes"
